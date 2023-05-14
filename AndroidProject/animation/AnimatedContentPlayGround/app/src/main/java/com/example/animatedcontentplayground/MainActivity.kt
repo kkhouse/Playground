40bf5a25fe56eccf7cef2019746d2ebc4ad4ed19
kkhouse <intelinside.7575@gmail.com>
2023-05-14 19:08:21 +0900

    animetion 途中

--- AndroidProject/animation/AnimatedContentPlayGround/app/src/main/java/com/example/animatedcontentplayground/MainActivity.kt
+++ AndroidProject/animation/AnimatedContentPlayGround/app/src/main/java/com/example/animatedcontentplayground/MainActivity.kt
@@ -4,206 +4,198 @@ package com.example.animatedcontentplayground
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedVisibility
+import androidx.annotation.DrawableRes
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.SizeTransform
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.animateDp
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.animation.with
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import com.example.animatedcontentplayground.ui.theme.AnimatedContentPlayGroundTheme
+import kotlinx.coroutines.delay
+
+data class ViewPoint(
+    val topLeft: DpOffset = DpOffset(0.dp,0.dp),
+    val width: Dp = 0.dp,
+    val height: Dp = 0.dp,
+    val data: SampleData = SampleData()
+)
+
+data class SampleData(
+    val title : String = "Title",
+    @DrawableRes val img : Int = R.drawable.ic_android_black_24dp
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AnimatedContentPlayGroundTheme {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(24.dp)
-                ) {
-
-                }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp)
+                    .background(Color.Cyan)
+            ) {
+                var isSelected by remember { mutableStateOf(false) }
+                var viewData by remember { mutableStateOf(ViewPoint()) }
+                SampleContainer(
+                    onTap = { data , viewPoint ->
+                        isSelected = isSelected.not()
+                        viewData = viewPoint.copy(data = data)
+                    }
+                )
+                SampleContainerAnimationBox(
+                    data = viewData,
+                    isVisible = isSelected,
+                    onBackPressed = { isSelected = false }
+                )
             }
         }
     }
 }
 
-@Preview
 @Composable
-fun Preview() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Sample3()
-    }
-}
-@Composable
-fun BoxScope.SimpleSample() {
-    var count by remember { mutableStateOf(0) }
-    Row(
-        modifier = Modifier.align(Alignment.Center)
-    ) {
-        Button(onClick = { count++ }) {
-            Text("Add")
-        }
-        AnimatedContent(
-            targetState = count,
-            label = ""
-        ) { targetCount ->
-            // Make sure to use `targetCount`, not `count`.
-            Text(text = "Count: $targetCount")
-        }
-    }
-}
-@Composable
-fun BoxScope.SimpleSample2() {
-    var count by remember { mutableStateOf(0) }
-    Row(
-        modifier = Modifier.align(Alignment.Center),
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-        Button(onClick = { count++ }) {
-            Text("Add")
-        }
-        AnimatedContent(
-            targetState = count,
-            transitionSpec = {
-                if (targetState > initialState) {
-                    slideInVertically { height -> height } + fadeIn() with
-                            slideOutVertically { height -> -height } + fadeOut()
-                } else {
-                    slideInVertically { height -> -height } + fadeIn() with
-                            slideOutVertically { height -> height } + fadeOut()
-                }.using(
-                    SizeTransform(clip = false)
-                )
-            }, label = ""
-        ) { targetCount ->
-            Text(text = "$targetCount")
-        }
-        Button(onClick = { count-- }) {
-            Text("Dec")
-        }
-    }
-}
+fun SampleContainer(
+    onTap: (SampleData, ViewPoint) -> Unit
+) {
+    val density = LocalDensity.current
+    val list = listOf(SampleData(),SampleData(),SampleData(),SampleData(),SampleData(),SampleData(),SampleData(),)
 
-@Composable
-fun BoxScope.SimpleSample3() {
-    var expanded by remember { mutableStateOf(false) }
-    Row(
-        modifier = Modifier.clickable{ expanded = !expanded }
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        AnimatedContent(
-            targetState = expanded,
-            transitionSpec = {
-                fadeIn(animationSpec = tween(150, 150)) with
-                        fadeOut(animationSpec = tween(150)) using
-                        SizeTransform { initialSize, targetSize ->
-                            if (targetState) {
-                                keyframes {
-                                    // Expand horizontally first.
-                                    IntSize(targetSize.width, initialSize.height) at 150
-                                    durationMillis = 300
-                                }
-                            } else {
-                                keyframes {
-                                    // Shrink vertically first.
-                                    IntSize(initialSize.width, targetSize.height) at 150
-                                    durationMillis = 300
-                                }
+        items(list) { data ->
+            var viewPoint by remember { mutableStateOf(ViewPoint()) }
+            Column(
+                modifier = Modifier.clickable { onTap(data, viewPoint) },
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ){
+                Text(text = data.title)
+                Box(
+                    modifier = Modifier
+                        .border(width = 1.dp, color = Color.Black)
+                        .size(64.dp)
+                        .onGloballyPositioned {
+                            with(density) {
+                                viewPoint = viewPoint.copy(
+                                    topLeft = DpOffset(
+                                        // TODO よくわからないが、周辺パディングの影響を受ける様子。ベタがきで引いておく。
+                                        x = it.positionInRoot().x.toDp() - 12.dp,
+                                        y = it.positionInRoot().y.toDp() - 12.dp
+                                    ),
+                                    width = it.size.width.toDp(),
+                                    height = it.size.height.toDp()
+                                )
                             }
                         }
-            }, label = ""
-        ) { targetExpanded ->
-            if (targetExpanded) {
-//                Expanded()
-            } else {
-//                ContentIcon()
+                        .background(Color.Magenta)
+                ){
+                    Image(
+                        modifier = Modifier.fillMaxSize(),
+                        painter = painterResource(
+                            id = R.drawable.ic_android_black_24dp
+                        ),
+                        contentDescription = "",
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun Sample3() {
-    var selected by remember { mutableStateOf(false) }
-// Animates changes when `selected` is changed.
-    val transition = updateTransition(
-        selected,
-        label = "",
+fun SampleContainerAnimationBox(
+    data : ViewPoint,
+    isVisible: Boolean,
+    content: @Composable BoxScope.() -> Unit = {},
+    onBackPressed: () -> Unit = {}
+) {
+    var visibilityContent by remember { mutableStateOf(true) }
+    var viewData by remember { mutableStateOf(data) }
+    val screenWidth = LocalConfiguration.current.screenWidthDp
+    val screenHeight = LocalConfiguration.current.screenHeightDp
+    val animateWidth by animateDpAsState(targetValue = viewData.width, label = "")
+    val animateOffsetX by animateDpAsState(targetValue = viewData.topLeft.x, label = "")
+    val animateOffsetY by animateDpAsState(targetValue = viewData.topLeft.y, label = "")
+    val animateHeight by animateDpAsState(targetValue = viewData.height, label = "",
+        finishedListener = {
+            if(!isVisible) { visibilityContent = false }
+        }
     )
-    val borderColor by transition.animateColor { isSelected ->
-        if (isSelected) Color.Magenta else Color.White
+
+    if(visibilityContent) {
+        Box(
+            modifier = Modifier
+                .size(width = animateWidth, height = animateHeight)
+                .offset(animateOffsetX, animateOffsetY)
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = viewData.data.img),
+                contentDescription = ""
+            )
+            // content()
+        }
     }
-    val elevation by transition.animateDp { isSelected ->
-        if (isSelected) 10.dp else 2.dp
-    }
-    Surface(
-        shape = RoundedCornerShape(8.dp),
-        border = BorderStroke(2.dp, borderColor),
-        modifier = Modifier
-            .clickable { selected = !selected }
-            .shadow(elevation)
-    ) {
-        Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
-            Text(text = "Hello, world!")
-            // AnimatedVisibility as a part of the transition.
-            transition.AnimatedVisibility(
-                visible = { targetSelected -> targetSelected },
-                enter = expandVertically(),
-                exit = shrinkVertically()
-            ) {
-                Text(text = "It is fine today.")
-            }
-            // AnimatedContent as a part of the transition.
-            transition.AnimatedContent { targetState ->
-                if (targetState) {
-                    Text(text = "Selected")
-                } else {
-                    Icon(imageVector = Icons.Default.Phone, contentDescription = "Phone")
-                }
-            }
+    LaunchedEffect(key1 = isVisible) {
+        if (isVisible) {
+            viewData.copy(
+                width = data.width, height = data.height, topLeft = data.topLeft,
+            ).also { viewData = it }
+            delay(200)
+            visibilityContent = true
+            viewData.copy(
+                width = data.width, height = data.height, topLeft = data.topLeft,
+            ).also { viewData = it }
+            delay(200)
+            viewData.copy(
+                width = screenWidth.dp, height = screenHeight.dp, topLeft = DpOffset(0.dp,0.dp),
+            ).also { viewData = it }
+        } else {
+            viewData.copy(
+                width = data.width, height = data.height, topLeft = data.topLeft,
+            ).also { viewData = it }
         }
     }
 
+    BackHandler {
+        onBackPressed()
+    }
 }
