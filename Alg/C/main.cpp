84aec23a0dd234a37172f2576d62f8a26742ba8e
GENMAI <intelinside.7575@gmail.com>
2023-06-14 07:28:22 +0900

    h

--- Alg/C/main.cpp
+++ Alg/C/main.cpp
@@ -1,21 +1,43 @@
 
 #include <iostream>
-#include <math.h>
-#include <vector>
-#include <cmath>
-#include <algorithm> // sort関数を使用するために必要
-#include <ctype.h>
+#include <map>
+#include <set>
+#include <algorithm> // sortなど
 using namespace std;
-#define repo(i,n) for(int i = 0; i<(int)n; i++);
+#include <cctype> // isUpperCase
+#include <utility> // pair
+#include <vector> // vector
 
+bool isUpperCase(char c) {
+    return std::isupper(c) != 0;
+}
 
 int main()
 {
-  int N,Y;
-  cin >> N >> Y;
-  vector<string> S(H);
-  for(auto& row : S) {
-    cin >> row;
+  string S;
+  cin >> S;
+  vector<string> words;
+  for (int i = 0; i < S.size();)
+  {
+    int j = i+1; // 2文字以上のため自身は含めず探索していく
+    while(j < S.size() && !isUpperCase(S[j])) {
+      j++;
+    }
+    string word = S.substr(i, j + 1 - i);
+    // 小文字変更
+    word[0] = tolower(word[0]);
+    word.back() = tolower(word.back());
+    words.push_back(word);
+    i = j + 1;
   }
-  
+  sort(words.begin(), words.end());
+  string result = "";
+  for (auto v : words)
+  {
+    // 大文字変更
+    v[0] = toupper(v[0]);
+    v.back() = toupper(v.back());
+    result += v;
+  }
+  cout << result << endl;
 }
